4b25521bfc23f91466fc5e0372cec6ad720a6d9e
TipparatFern <tipparat.0690@mail.kmutt.ac.th>
2021-02-15 20:42:57 +0700

    edit  function

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,27 +1,11 @@
-const class_x ='x'
-const class_o ='o'
-
-
-
-/*Start game from class x by click */
 function clicktoPlay(){
-    const image = document.querySelector('img');
+    const image = document.querySelectorAll('img');
     image.src = 'https://www.shareicon.net/data/256x256/2016/03/19/736268_error_512x512.png';
     image.removeEventListener('click',clicktoPlay);
  }
  
- const image = document.querySelector('img');
+ const image = document.querySelectorAll('img');
  image.addEventListener('click',clicktoPlay);
-
- /*class o random
- function randomtoPlay(){
-    const image = document.querySelector('img');
-    image.src = 'https://i.dlpng.com/static/png/1185263-open-o-png-2000_2000_preview.png';
-    image.removeEventListener('random',randomtoPlay);
- }
- 
- const image = document.querySelector('img');
- image.addEventListener('random',randomtoPlay);*/
 
  
  
