2864692b3ec22542ad4d218661efae0fc047ccc7
TipparatFern <tipparat.0690@mail.kmutt.ac.th>
2021-03-02 17:40:17 +0700

    edit charactor

--- your_fav.js
+++ your_fav.js
@@ -1,4 +1,4 @@
-charac = prompt('What is your favorite charator?');
-console.log( charac +' is your favorite charator.');
-color = prompt('What is the main color of '+ charac);
+let charac = prompt('What is your favorite charactor?');
+console.log( charac +' is your favorite charactor.');
+let color = prompt('What is the main color of '+ charac);
 console.log( charac +' is '+ color + '.');
